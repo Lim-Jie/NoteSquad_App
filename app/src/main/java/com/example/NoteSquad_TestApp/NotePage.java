7340c45a53395f054a247fd89a2747513414dda9
Hang We Hong <116018409+HangWeHong@users.noreply.github.com>
2023-12-01 15:38:04 +0800

    NoteSharingDraft (#1)
    Co-authored-by: Hang We Hong <s2175788@siswa.um.edu.my>

---
+++ app/src/main/java/com/example/NoteSquad_TestApp/NotePage.java
@@ -0,0 +1,30 @@
+package com.example.NoteSquad_TestApp;
+
+import java.util.ArrayList;
+
+public class NotePage {
+    String subject;
+    ArrayList<Notes> notesList;
+
+
+    public NotePage(String subject, ArrayList<Notes> notesList) {
+        this.subject = subject;
+        this.notesList = notesList;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public ArrayList<Notes> getNotesList() {
+        return notesList;
+    }
+
+    public void setNotesList(ArrayList<Notes> notesList) {
+        this.notesList = notesList;
+    }
+}
