ec29510064a46a31d29f18c6cf50e3c815028d53
Joshua Chew Jay Han <joshuachew8118@gmail.com>
2024-01-06 18:44:30 +0800

    Add forum
    Signed-off-by: Joshua Chew Jay Han <joshuachew8118@gmail.com>

--- app/src/main/java/com/example/NoteSquad_TestApp/HomePage.java
+++ app/src/main/java/com/example/NoteSquad_TestApp/HomePage.java
@@ -70,7 +70,7 @@ public final class HomePage extends AppCompatActivity {
                 return true;
             }
             else if( R.id.item_4==no){ //ForumFragment Button 3
-                replaceFragment(new forumFragment());
+                replaceFragment(new ForumFragment());
                 return true;
             }
             else if( R.id.item_5==no){ //ProfileFragment Button 4
