d94cbb452435a2376dc51a27a2c717a73de8f009
Lim-Jie <lijiebiz@gmail.com>
2024-01-06 17:12:19 +0800

    Latest Update Cyril Code Inclusive

---
+++ app/build.gradle.kts
@@ -0,0 +1,76 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
+
+}
+
+android {
+    namespace = "com.example.NoteSquad_TestApp"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.NoteSquad_TestApp"
+        minSdk = 29
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildFeatures {
+       viewBinding= true
+        dataBinding= true
+
+    }
+
+/*
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+
+ */
+
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+
+        //1_8 old one
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    implementation ("com.github.bumptech.glide:glide:4.16.0")
+    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation ("androidx.cardview:cardview:1.0.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-firestore:24.9.1")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+    implementation ("com.google.firebase:firebase-auth:21.0.1")
+    implementation(platform("com.google.firebase:firebase-bom:32.5.0"))
+    implementation("com.google.firebase:firebase-analytics")
+    implementation ("com.google.android.gms:play-services-auth:20.7.0")
+
+    
+    implementation("de.hdodenhof:circleimageview:3.0.0")
+    implementation ("androidx.cardview:cardview:1.0.0")
+}
+
---
+++ app/src/main/java/com/example/NoteSquad_TestApp/profileFragment.java
@@ -0,0 +1,137 @@
+package com.example.NoteSquad_TestApp;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.ListenerRegistration;
+
+
+public class profileFragment extends Fragment {
+    FirebaseFirestore firestore;
+    GoogleSignInAccount account;
+    private ListenerRegistration listenerRegistration;
+    TextView profileBiography;
+    TextView profileUniversity;
+    TextView profileUsername;
+    TextView profileContributions;
+    TextView profileEngagements;
+    TextView profileConnections;
+    TextView profileEmail;
+    Button updateUserButton;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view=  inflater.inflate(R.layout.fragment_profile, container, false);
+
+        //INITIALIZE FIRESTORE
+         firestore=FirebaseFirestore.getInstance();
+
+
+         //INITIALIZE OBJECTS AND VIEW COMPONENTS
+         updateUserButton= (Button)view.findViewById(R.id.updateUserDetailsButton);
+         profileUsername=(TextView)view.findViewById(R.id.profileNameUser);
+         profileUniversity=(TextView)view.findViewById(R.id.universityPP);
+         profileBiography=(TextView)view.findViewById(R.id.biographyPP);
+         profileContributions=(TextView)view.findViewById(R.id.ContributionsVal);
+         profileEngagements=(TextView)view.findViewById(R.id.EngagementVal);
+         profileConnections=(TextView)view.findViewById(R.id.ConnectionsVal);
+         profileEmail=(TextView)view.findViewById(R.id.profileEmail);
+
+
+
+
+        //LISTENER BUTTONS
+         updateUserButton.setOnClickListener(v->{
+             FragmentManager fm = getActivity().getSupportFragmentManager();
+             FragmentTransaction ft = fm.beginTransaction();
+             ft.replace(R.id.constraintLayoutFragment, new updateuserdetails());
+             ft.commit();
+
+             /* CAN ALSO DO THIS METHOD
+
+             HomePage activity  = (HomePage) getActivity();
+             activity.replaceFragment(new updateuserdetails());
+              */
+         });
+
+
+
+         //SET FIRESTORE PROFILE VALUES INTO XML
+         getFireStoreData("Users",getEmail(), "username",profileUsername);
+         getFireStoreData("Users", getEmail(), "university", profileUniversity);
+         getFireStoreData("Users", getEmail(), "biography", profileBiography);
+         getFireStoreData("Users",getEmail(),"contributions",profileContributions);
+         getFireStoreData("Users",getEmail(),"engagements",profileEngagements);
+         getFireStoreData("Users",getEmail(),"connections",profileConnections);
+         getFireStoreData("Users",getEmail(),"email",profileEmail);
+
+
+         //UNDERLINE THE UNIVERSITY TEXTVIEW ENTIRELY
+
+
+        return view;
+    }
+
+
+
+    private void getFireStoreData(String collection, String document, String FieldName, TextView textView){
+        listenerRegistration = firestore.collection(collection)
+                .document(document)
+                .addSnapshotListener((documentSnapshot, e) -> {
+                    if (e != null) {
+                        return; //IF THERE IS ERRORS
+                    }
+                    if (documentSnapshot != null && documentSnapshot.exists()) {
+                        Object value = documentSnapshot.get(FieldName);
+
+                        if (value != null) {
+                            if (value instanceof Number) {
+                                textView.setText(String.valueOf((Number) value));
+                            } else if (value instanceof String) {
+                                // If the value is a string, you can directly set it
+                                textView.setText((String) value);
+                            }
+                        }
+                    }
+                });
+    }
+
+
+
+
+
+    public String getEmail(){
+        account = GoogleSignIn.getLastSignedInAccount(getContext());
+        if(account!=null){
+            return account.getEmail();}
+
+        return null;
+    }
+
+
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Remove the Firestore snapshot listener when the activity is destroyed
+        if (listenerRegistration != null) {
+            listenerRegistration.remove();
+        }
+    }
+}
