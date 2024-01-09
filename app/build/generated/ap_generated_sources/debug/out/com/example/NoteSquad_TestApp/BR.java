65979236ea1882e63feb996deb05fc591d64275f
Lim-Jie <lijiebiz@gmail.com>
2024-01-10 00:37:53 +0800

    Push All gradle files

---
+++ app/build/generated/ap_generated_sources/debug/out/com/example/NoteSquad_TestApp/BR.java
@@ -0,0 +1,5 @@
+package com.example.NoteSquad_TestApp;
+
+public class BR {
+  public static final int _all = 0;
+}
---
+++ app/build/generated/data_binding_trigger/debug/com/example/NoteSquad_TestApp/DataBindingTriggerClass.java
@@ -0,0 +1,4 @@
+package com.example.NoteSquad_TestApp;
+
+@androidx.databinding.BindingBuildInfo
+public class DataBindingTriggerClass {}
